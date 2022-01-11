35a6f1c021d1da55bbe9959499fe889e2d52eb51
ANNE TRINH <a.trinh@garvan.org.au>
2022-01-11 11:08:19 +1100

    include option for non canonical output

--- oncokb/vepVCF2maf4Oncokb.R
+++ oncokb/vepVCF2maf4Oncokb.R
@@ -1,12 +1,10 @@
 #!/usr/bin/Rscript
-"usage: \n vepVCF2maf4Oncokb.R [--vcffile=<file> --outputfile=<string> --sampleName=<string> --AAlist=<file> --protein=<boolean> --pfam=<file> --pirsf=<file> ]
+"usage: \n vepVCF2maf4Oncokb.R [--vcffile=<file> --outputfile=<string> --sampleName=<string> --AAlist=<file> --canonical=<boolean> ]
 \n options:\n --vcffile=<file> vcffile that has been annotated using vep.
 \n --outputfile=<string> output directory
 \n --sampleName=<string> If sample list has more than 1 sample, include a csv file with all identifiers, otherwise set as 'NULL' [default: NULL]
 \n --AAlist=<file> File containing aminoacid 3 letter and 1 letter conversion
-\n --protein=<protein> Do you want to annotate the protein names [default: FALSE]
-\n --pfam=<file> File containing pfam annotations [default: NULL]
-\n --pirsf=<file> File containing psirf annotations [default: NULL] " -> doc
+\n --canonical=<boolean> Need to separate out canonical values if not run " -> doc
 
 library("docopt")
 opts <- docopt(doc)
@@ -17,7 +15,7 @@ opts <- docopt(doc)
 ## run with the options:
 ## --domains --af_gnomad --check_existing --fields CANONICAL,Consequence,Codons,Amino_acids,Gene,SYMBOL,Feature,EXON,PolyPhen,SIFT,Protein_position,BIOTYPE,Feature_type,cDNA_position,CDS_position,Existing_variation,DISTANCE,STRAND,CLIN_SIG,LoF_flags,LoF_filter,LoF,RadialSVM_score,RadialSVM_pred,LR_score,LR_pred,CADD_raw,CADD_phred,Reliability_index,DOMAINS,HGVSc,HGVSp,gnomAD_AF,PHENO
 
-vcf2MAF=function(vcffile,outputfile,sampleName, AAlist, protein=F, pfam, pirsf){
+vcf2MAF=function(vcffile,outputfile,sampleName, AAlist, canonical=F){
   library(vcfR)
   library(matrixStats)
   AAlist=read.csv(AAlist)
@@ -50,26 +48,32 @@ vcf2MAF=function(vcffile,outputfile,sampleName, AAlist, protein=F, pfam, pirsf){
   print('Extract information on CSQ')
   ann <- extract.info(inputFile, element='CSQ', as.numeric=F)
   annsplit=sapply(ann, function(x) strsplit(x, "\\||,"))
-  names(annsplit)=paste("X",c(1:length(annsplit)))
   print('Create new data table')
-  AllData=do.call(rbind, annsplit)
-  colnames(AllData)=SNames[1:ncol(AllData)]
-  # 
-  # yesgrep=sapply(annsplit, function(x) grep("^YES$", x)) ## need to make sure protein sequences are not grepped
-  # tx=sapply(yesgrep, length)
-  # 
-  # yesgrep2<-lapply(yesgrep, function(x) {ifelse(length(x)==0, x<-lxa, x); x})
-  # tx2=sapply(yesgrep2, length)
-  # 
-  # csq3=sapply(1:length(annsplit), function(x) annsplit[[x]][(yesgrep2[[x]][1]-lxa+1): (yesgrep2[[x]][1]+lxb)]) ## issue line 932
-  # csq3=t(csq3)
-  # colnames(csq3)=SNames
-  # 
-
-  ## Pfam annotation:
-    
-  ##ann <-sapply(SearchFeat, function(x) extract.info(inputFile, element=x, as.numeric=F))
+  ncounts=sapply(annsplit, length)
   
+  print(SNames)
+  
+  if (length(unique(ncounts))>1){ 
+    canonical = T }
+  
+  if (canonical==T){
+    lxa=which(SNames=="CANONICAL")
+    lxb=length(SNames)-lxa
+    print(lxa)
+    print(lxb)
+    yesgrep=sapply(annsplit, function(x) grep("^YES$", x)) ## need to make sure protein sequences are not grepped
+    tx=sapply(yesgrep, length)
+    yesgrep2<-lapply(yesgrep, function(x) {ifelse(length(x)==0, x<-lxa, x); x})
+    tx2=sapply(yesgrep2, length)
+    csq3=sapply(1:length(annsplit), function(x) annsplit[[x]][(yesgrep2[[x]][1]-lxa+1): (yesgrep2[[x]][1]+lxb)]) ## issue line 932
+    AllData=t(csq3)
+    colnames(AllData)=SNames
+  } else {
+    names(annsplit)=paste("X",c(1:length(annsplit)))
+    AllData=do.call(rbind, annsplit)
+    colnames(AllData)=SNames[1:ncol(AllData)]    
+  }
+
   ## Create HGVSp short:
   xa=strsplit(AllData[ ,match("HGVSp", colnames(AllData))], ":p\\.")
   ax2=sapply(xa, function(x) x[2])
@@ -91,32 +95,7 @@ vcf2MAF=function(vcffile,outputfile,sampleName, AAlist, protein=F, pfam, pirsf){
     
    HGVSc=sapply(strsplit(AllData[, "HGVSc"], ":"), function(x) x[2])
    AllData=cbind(AllData[ ,-match(c("HGVSc", "HGVSp"), colnames(AllData))],HGVSp_Short, HGVSp_ENS, HGVSp,HGVSc_ENS, HGVSc)
-
-   # redo check to run protein
-   proteinR = as.logical(protein) & !is.null(pirsf) & !is.null(pfam)
-   #print(proteinR)
-   #print(protein)
    
-   if (proteinR==T){
-  print('converting proteins')
-  ## pfam entry
-   pfamI=read.delim(pfam, header=F, sep="\t")
-   pirsfinfo=read.delim(pirsf, sep=")", header=F, quote="")
-   pirsfinfo$V3=substr(pirsfinfo$V1, 2, 12)
-
-  n2=regmatches( AllData[ ,match("DOMAINS", colnames(AllData))], 
-                 regexec("PF[0-9]{5}", AllData[ ,match("DOMAINS", colnames(AllData))]))
-  n2pfam=unlist({n2[sapply(n2, length)==0] <- NA; n2})
-  pfamO=as.character(pfamI$V5[match(n2pfam, pfamI$V1)])
-  
-  n2=regmatches( AllData[ ,match("DOMAINS", colnames(AllData))], 
-                 regexec("PIRSF[0-9]{6}", AllData[ ,match("DOMAINS", colnames(AllData))]))
-  n2=unlist({n2[sapply(n2, length)==0] <- NA; n2})
-  pirsfO=as.character(pirsfinfo$V3[match(n2,pirsfinfo$V1)])
-  
-  AllData=cbind(AllData, pfamid=n2pfam, pfam=pfamO, pirsf=pirsfO, pirsfid=n2)
-}
-
   data.out=data.frame(Tumor_Sample_Barcode=sampleName, vcfFix, AllData, adv, dp, vaf)
   colnames(data.out)[which(colnames(data.out)=="SYMBOL")]="Hugo_Symbol"
   
@@ -128,7 +107,7 @@ vcf2MAF=function(vcffile,outputfile,sampleName, AAlist, protein=F, pfam, pirsf){
 #vcf2MAF(vcffile, "~/Downloads/abra2_vc/vepcall/rerun_protein.maf", sampleName =  "ER099_MEL4N", AAlist="~/Documents/ER_pilot/annotations/AminoAcid_table.csv",protein=T,pfam="~/Documents/ER_pilot/annotations/Pfam-A.clans.33.1.tsv", pirsf = "~/Documents/ER_pilot/annotations/pirsfinfo.cat")
 
 ##print(opts)
-vcf2MAF(opts$vcffile, opts$outputfile, opts$sampleName, opts$AAlist, opts$protein, opts$pfam, opts$pirsf)
+vcf2MAF(opts$vcffile, opts$outputfile, opts$sampleName, opts$AAlist, opts$canonical)
 
 
 
